7d8faeb3a6690f5e02e6c4396e4b560ac2d4ba65
felipeschirmann@gmail.com <felipeschirmann@gmail.com>
2022-01-10 15:57:44 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Página Principal </h1> 
+      <Navbar />
     </div>
   );
 }
