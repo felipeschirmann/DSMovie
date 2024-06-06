e6165cd98bb25dec73b3ab535f146b10f23ff3e0
Felipe Schirmann <contato@felipeschirmann.dev.br>
2024-06-06 19:25:16 -0300

    Update backend SpringBoot version and spring security

--- backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsmovie.config;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,35 +10,38 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
 	@Autowired
 	private Environment env;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-			http.headers().frameOptions().disable();
-		}
 
-		http.cors().and().csrf().disable();
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
+			http.authorizeHttpRequests((requests) -> requests.requestMatchers(toH2Console()).permitAll() // <-
+			).csrf((protection) -> protection.ignoringRequestMatchers(toH2Console()) // <-
+			);
+		}
+		http.authorizeHttpRequests(
+				(requests) -> requests.requestMatchers("/**").permitAll().anyRequest().authenticated())
+				.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
+				.cors((cors) -> cors.configurationSource(profileConfigurationSource()));
+		return http.build();
 	}
 
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+	CorsConfigurationSource profileConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList(env.getProperty("cors.allow.frontend")));
 		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
--- backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dsmovie.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
-import javax.persistence.Embeddable;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 
 @Embeddable
 public class ScorePK implements Serializable{
@@ -40,6 +42,21 @@ public class ScorePK implements Serializable{
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(movie, user);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ScorePK other = (ScorePK) obj;
+		return Objects.equals(movie, other.movie) && Objects.equals(user, other.user);
+	}
 }
